aa0345daaf5bb6b933a05a042ac9c886a0db8ecb
Marmoroju <78829079+Marmoroju@users.noreply.github.com>
2024-03-16 16:09:32 -0300

    Update BACKUP_DIA_SEMANA.bat

--- Backup/BACKUP_DIA_SEMANA.bat
+++ Backup/BACKUP_DIA_SEMANA.bat
@@ -13,32 +13,32 @@ if %dow% == Friday goto Friday
 if %dow% == Saturday goto Saturday
 if %dow% == Sunday goto Sunday
 
+:Sunday 
+xcopy %ORIGEM% %DESTINO% /E /C /I /Y
+echo BACKUP DE DOMINGO REALIZADO COM SUCESSO EM %DATE% as %TIME% >> %LOG%
+
 :Monday 
 xcopy %ORIGEM% %DESTINO% /E /C /I /Y
-echo BACKUP de %dow% REALIZADO COM SUCESSO EM %DATE% as %TIME% >> %LOG%
+echo BACKUP DE SEGUNDA-FEIRA REALIZADO COM SUCESSO EM %DATE% as %TIME% >> %LOG%
 
 :Tuesday 
 xcopy %ORIGEM% %DESTINO% /E /C /I /Y
-echo BACKUP de %dow% REALIZADO COM SUCESSO EM %DATE% as %TIME% >> %LOG%
+echo BACKUP DE TERÇA-FEIRA REALIZADO COM SUCESSO EM %DATE% as %TIME% >> %LOG%
 
 :Wednesday 
 xcopy %ORIGEM% %DESTINO% /E /C /I /Y
-echo BACKUP de %dow% REALIZADO COM SUCESSO EM %DATE% as %TIME% >> %LOG%
+echo BACKUP DE QUARTA-FEIRA REALIZADO COM SUCESSO EM %DATE% as %TIME% >> %LOG%
 
 :Thursday 
 xcopy %ORIGEM% %DESTINO% /E /C /I /Y
-echo BACKUP de %dow% REALIZADO COM SUCESSO EM %DATE% as %TIME% >> %LOG%
+echo BACKUP DE QUINTA-FEIRA REALIZADO COM SUCESSO EM %DATE% as %TIME% >> %LOG%
 
 :Friday 
 xcopy %ORIGEM% %DESTINO% /E /C /I /Y
-echo BACKUP de %dow% REALIZADO COM SUCESSO EM %DATE% as %TIME% >> %LOG%
+echo BACKUP DE SEXTA-FEIRA REALIZADO COM SUCESSO EM %DATE% as %TIME% >> %LOG%
 
 :Saturday 
 xcopy %ORIGEM% %DESTINO% /E /C /I /Y
-echo BACKUP de %dow% REALIZADO COM SUCESSO EM %DATE% as %TIME% >> %LOG%
+echo BACKUP DE SÁBADO REALIZADO COM SUCESSO EM %DATE% as %TIME% >> %LOG%
 
-:Sunday 
-xcopy %ORIGEM% %DESTINO% /E /C /I /Y
-echo BACKUP de %dow% REALIZADO COM SUCESSO EM %DATE% as %TIME% >> %LOG%
-
-pause
+exit
