c12e069bf52a866c462a93cfb0c857fdc2cab4f9
Marcos Mourão Rodrigues Junior <78829079+Marmoroju@users.noreply.github.com>
2024-09-05 18:11:00 -0300

    updates

---
+++ restart_explorer.bat
@@ -0,0 +1,8 @@
+REM taskkill: Comando para encerrar processos.
+REM /f: Força o encerramento do processo
+REM /im: Especifica o nome da imagem do processo a ser encerrado.
+REM Por exemplo, para encerrar o Google Chrome, você usaria: 
+REM taskkill /f /im chrome.exe
+
+taskkill /f /im explorer.exe
+start explorer.exe
---
+++ shades.bat
@@ -0,0 +1,10 @@
+mkdir v:\CachedHLSLShaders
+mkdir v:\ShaderCacheD3D11
+mkdir v:\ShaderCacheD3D11_GI
+mkdir v:\minimap
+mkdir v:\ShaderCacheD3D12
+mklink /D "C:\Users\Marcos\AppData\Roaming\Path of Exile\ShaderCacheD3D12" v:\ShaderCacheD3D12
+mklink /D "C:\Program Files (x86)\Steam\steamapps\common\Path of Exile\CachedHLSLShaders" v:\CachedHLSLShaders
+mklink /D "C:\Program Files (x86)\Steam\steamapps\common\Path of Exile\ShaderCacheD3D11" v:\ShaderCacheD3D11
+mklink /D "C:\Program Files (x86)\Steam\steamapps\common\Path of Exile\ShaderCacheD3D11_GI" v:\ShaderCacheD3D11_GI
+mklink /D "C:\Users\Marcos\AppData\Roaming\Path of Exile\Minimap" v:\minimap
